16f3af770902784cf90eeadfdbe9115b3e7e140e
Michał Ziober <michalziober08@gmail.com>
2023-01-27 08:17:35 +0100

    added showing orders, ordering, validation and few smaller things

--- validator.py
+++ validator.py
@@ -1,13 +1,12 @@
-def validate_register_data_corectness(login, password, repeated_password):
-    if(login is not None and password is not None and repeated_password is not None):
-        if(len(login) > 0 and len(password) > 0):
-            if(password != repeated_password):
-                return False
-            
-        return len(login) > 0 and len(password) > 0 
-    return False
+import re
+from flask import flash
+# string that contains only letters and numbers
+login_pattern = "^[a-zA-Z0-9]+$"
+# at least 8 characters, one uppercase letter, one lowercase letter, one number and one special character
+password_pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
 
-# def validate_login_data_corectness(login, password):
-#     if(login is not None and password is not None):
-#         return(len(login) > 0 and len(password) > 0)
-#     return False
+def validate_login_and_password(login, password):
+    if re.match(login_pattern, login) and re.match(password_pattern, password):
+        return True
+    else:
+        return False
